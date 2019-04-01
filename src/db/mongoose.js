5316b727fcceae16792fe5a3567bfd1ba583bc9e
topetin <carballalmariana@gmail.com>
2019-04-01 18:39:03 -0300

    configuration of endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,47 +1,18 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
-mongoose.connect('mongodb://127.0.0.1/task-manager-api', {
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error ('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number
-    }
-})
-
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
-})
-
-const me = new User({
-    name: 'Mariana',
-    email: 'alala'
-})
-
-
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
 })
